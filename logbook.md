4f80c490cb7eacae23a2738df0c9615ca6e0003a
unknown <towsurapong@gmail.com>
2019-01-15 23:57:28 +0700

    Commit after test http get is fail

--- logbook.md
+++ logbook.md
@@ -5,3 +5,17 @@
 ### วิธีเขียน Angular element
 
 https://scotch.io/tutorials/build-a-reusable-component-with-angular-elements#toc-what-you-ll-need-to-get-started-with-angular-elements
+
+## 2019-01-15
+
+### การใช้ HttpClient
+
+https://blog.angular-university.io/angular-http/
+
+### การใช้ iframe
+
+https://www.w3schools.com/tags/tag_iframe.asp
+
+### ดึ่งค่าจาก iframe แบบ Angular
+
+https://stackoverflow.com/questions/36513929/change-the-property-of-element-inside-iframe-in-angular-2
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'joox-angular-element';
+export class AppComponent implements OnInit {
+  
+  constructor(private http:HttpClient) {
+
+  }
+  
+  ngOnInit(): void {
+    this.title = "Hello world";
+
+    // this.http.get("https://www.joox.com/th/th/single/YqaflYh+WnO0Iz2Mrp2_GA==").toPromise().then( (data) => {
+    //   console.log(data);
+    // })
+
+  }
+  title = '123';
 }
